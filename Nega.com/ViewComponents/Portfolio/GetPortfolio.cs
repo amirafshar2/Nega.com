e214695524615a58340db3da9c8ae6f6f4c86354
afsha <afsha@AMIR>
2024-05-25 18:31:15 +0300

    UPDATE PORTFOLİO UI

---
+++ Nega.com/ViewComponents/Portfolio/GetPortfolio.cs
@@ -0,0 +1,16 @@
+﻿using BLL.Concrate;
+using DAL.EntityFrameWork;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Negacom.ViewComponents.Portfolio
+{
+    public class GetPortfolio : ViewComponent
+    {
+        PortfolioManager _portfoliobll = new PortfolioManager(new EFPortfolioRepository());
+        public IViewComponentResult Invoke()
+        {
+            var value = _portfoliobll.Porfoliobaycategory();
+            return View(value);
+        }
+    }
+}
---
+++ Nega.com/ViewComponents/Portfolio/GetPortfolioCategory.cs
@@ -0,0 +1,16 @@
+﻿using BLL.Concrate;
+using DAL.EntityFrameWork;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Negacom.ViewComponents.Portfolio
+{
+    public class GetPortfolioCategory: ViewComponent
+    {
+        PortfolioCategoryManager _portfoliocategorybll =new PortfolioCategoryManager(new EFPortfoiloCategoryRepository());
+        public IViewComponentResult Invoke()
+        {
+            var val= _portfoliocategorybll.GetAll();
+            return View(val);
+        }
+    }
+}
