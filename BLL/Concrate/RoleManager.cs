48885fb5b329d7acad0e730d8214a1042f7da99f
Amir AFSHAR <amir_afshar1@outlook.com>
2024-05-31 18:30:34 +0300

    update role create,

---
+++ BLL/Concrate/RoleManager.cs
@@ -0,0 +1,46 @@
+﻿using BE;
+using BLL.Abstract;
+using DAL.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Concrate
+{
+    public class RoleManager : IRoleService
+    {
+        IRoleDAL _roledal;
+
+        public RoleManager(IRoleDAL roledal)
+        {
+            _roledal = roledal;
+        }
+
+        public void Add(UserRolee t)
+        {
+            _roledal.Add(t);
+        }
+
+        public void Delete(UserRolee t)
+        {
+            _roledal.Delete(t);
+        }
+
+        public List<UserRolee> GetAll()
+        {
+            return _roledal.GetAll();
+        }
+
+        public UserRolee GetById(int id)
+        {
+            return _roledal.GetBayId(id);
+        }
+
+        public void Update(UserRolee t)
+        {
+            _roledal.Update(t);
+        }
+    }
+}
---
+++ Nega.com/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,52 @@
+﻿using BE;
+using BLL.Concrate;
+using DAL.Context;
+using DAL.EntityFrameWork;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Negacom.Areas.Admin.Controllers
+{
+
+
+
+
+
+    [Area("Admin")]
+    public class RoleController : Controller
+    {
+        DB db = new DB();
+        RoleManager _rolebll = new RoleManager(new EFUserRoleeRepository());
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(UserRolee u)
+        {
+            if (u.Name == null)
+            {
+                ModelState.AddModelError("", "Connot be left bland the name");
+                return View(u);
+            }
+            else
+            {
+                u.Status = true;
+                _rolebll.Add(u);
+                return View();
+            }
+        }
+        [HttpPost]
+        public IActionResult UpdateStatus(int id, bool status)
+        {
+            var comment = _rolebll.GetById(id);
+            if (comment != null)
+            {
+                comment.Status = status;
+                _rolebll.Update(comment);
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+    }
+}
