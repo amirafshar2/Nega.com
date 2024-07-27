89071330952e2c8a122bedb7371b262d7a055a18
Amir AFSHAR <152279280+amirafshar2@users.noreply.github.com>
2024-07-27 11:37:13 +0200

    Blog Detale Update

---
+++ Nega.com/Controllers/BlogController.cs
@@ -0,0 +1,15 @@
+﻿using BLL.Concrate;
+using DAL.EntityFrameWork;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Negacom.Controllers
+{
+    public class BlogController : Controller
+    {
+        BlogManager _blogbll = new BlogManager(new EFBlogRepository());
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
