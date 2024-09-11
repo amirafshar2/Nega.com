a63fe7649de247c6fb1338baf19ecdfde3f0edf4
Amir AFSHAR <152279280+amirafshar2@users.noreply.github.com>
2024-09-11 20:32:35 +0200

    Blog List Page Front

---
+++ Nega.com/Controllers/BlogListController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Negacom.Controllers
+{
+    public class BlogListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Nega.com/ViewComponents/Blog/GetBlogList.cs
@@ -0,0 +1,19 @@
+﻿using BLL.Concrate;
+using DAL.EntityFrameWork;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Negacom.ViewComponents.Blog
+{
+	public class GetBlogList : ViewComponent
+	{
+		BlogManager _blogbll = new BlogManager(new EFBlogRepository());
+		public IViewComponentResult Invoke()
+		{
+			var q = _blogbll.GetAll();
+			q.Reverse();
+			q.Take(10);
+			return View(q);
+		}
+	}
+}
